39c412c5b07eefd134c4c0c1ea49c3d8f57b181b
Delon Newman <delnewman@salud.unm.edu>
2021-02-27 21:42:19 -0700

    Update hash delegator

--- lib/literature_requests/entity.rb
+++ lib/literature_requests/entity.rb
@@ -1,27 +1,9 @@
 module LiteratureRequests
   class Entity < HashDelegator
+    transform_keys(&:to_sym)
+
     def self.[](attributes = EMPTY_HASH)
-      new(attributes.transform_keys(&:to_sym))
-    end
-
-    def initialize(attributes = EMPTY_HASH)
-      super(attributes)
-    end
-
-    def hash
-      @hash.hash
-    end
-
-    def eql?(other)
-      @hash.hash == other.hash
-    end
-
-    def ==(other)
-      other.is_a?(self.class) && @hash.hash == other.hash
-    end
-
-    def ===(other)
-      @hash.hash == other.hash
+      new(attributes)
     end
   end
 end
